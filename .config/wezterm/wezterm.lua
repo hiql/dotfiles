3dababe361c4db5f7c24ce681705900fb4083c2d
hiql <qiu_lin@163.com>
2024-02-08 19:14:13 +0800

    Create wezterm.lua

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,487 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+local function font(opts)
+	return wezterm.font_with_fallback({
+		opts,
+		{ family = "JetBrainsMono Nerd Font Mono", scale = 0.9 },
+	})
+end
+
+config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+config.default_workspace = "devdog"
+
+config.font = font({ family = "MesloLGSDZ Nerd Font Mono" })
+config.font_size = 12.0
+config.harfbuzz_features = { "zero", "cv05", "cv02", "ss05", "ss04" }
+
+config.initial_cols = 180
+config.initial_rows = 48
+
+config.window_padding = {
+	left = "0.5cell",
+	right = "0.5cell",
+	top = "0.25cell",
+	bottom = "0.25cell",
+}
+
+config.window_frame = {
+	-- The font used in the tab bar.
+	-- Roboto Bold is the default; this font is bundled
+	-- with wezterm.
+	-- Whatever font is selected hqere, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
+	font = font({ family = "MesloLGSDZ Nerd Font Mono", weight = "Bold" }),
+
+	-- The size of the font in the tab bar.
+	-- Default to 10. on Windows but 12.0 on other systems
+	font_size = 12.0,
+}
+
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 20
+config.adjust_window_size_when_changing_font_size = false
+config.enable_scroll_bar = false
+config.scrollback_lines = 50000
+config.cell_width = 0.9
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.show_tab_index_in_tab_bar = true
+config.tab_max_width = 32
+config.hide_tab_bar_if_only_one_tab = true
+config.command_palette_font_size = 12
+
+config.color_scheme = "Catppuccin Macchiato (Gogh)"
+config.bold_brightens_ansi_colors = true
+
+config.colors = {
+	tab_bar = {
+		-- The color of the strip that goes along the top of the window
+		-- (does not apply when fancy tab bar is in use)
+		background = "#0b0022",
+
+		-- The active tab is the one that has focus in the window
+		active_tab = {
+			-- The color of the background area for the tab
+			bg_color = "#3c1361",
+			-- The color of the text for the tab
+			fg_color = "#c0c0c0",
+
+			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+			-- label shown for this tab.
+			-- The default is "Normal"
+			intensity = "Bold",
+
+			-- Specify whether you want "None", "Single" or "Double" underline for
+			-- label shown for this tab.
+			-- The default is "None"
+			underline = "None",
+
+			-- Specify whether you want the text to be italic (true) or not (false)
+			-- for this tab.  The default is false.
+			italic = false,
+
+			-- Specify whether you want the text to be rendered with strikethrough (true)
+			-- or not for this tab.  The default is false.
+			strikethrough = false,
+		},
+
+		-- Inactive tabs are the tabs that do not have focus
+		inactive_tab = {
+			bg_color = "#1b1032",
+			fg_color = "#808080",
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `inactive_tab`.
+		},
+
+		-- You can configure some alternate styling when the mouse pointer
+		-- moves over inactive tabs
+		inactive_tab_hover = {
+			bg_color = "#3b3052",
+			fg_color = "#909090",
+			italic = true,
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `inactive_tab_hover`.
+		},
+
+		-- The new tab button that let you create new tabs
+		new_tab = {
+			bg_color = "#1f232e",
+			fg_color = "#c0c0c0",
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `new_tab`.
+		},
+
+		-- You can configure some alternate styling when the mouse pointer
+		-- moves over the new tab button
+		new_tab_hover = {
+			bg_color = "#3b3052",
+			fg_color = "#909090",
+			italic = true,
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `new_tab_hover`.
+		},
+	},
+}
+
+-- keymaps
+
+config.disable_default_key_bindings = true
+-- config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
+local keymaps = {
+	{ key = "N", mods = "SHIFT|CMD", action = act.SpawnWindow },
+
+	{ key = "[", mods = "CMD", action = act.ActivateTabRelative(-1) },
+	{ key = "]", mods = "CMD", action = act.ActivateTabRelative(1) },
+	{ key = "1", mods = "CMD", action = act.ActivateTab(0) },
+	{ key = "2", mods = "CMD", action = act.ActivateTab(1) },
+	{ key = "3", mods = "CMD", action = act.ActivateTab(2) },
+	{ key = "4", mods = "CMD", action = act.ActivateTab(3) },
+	{ key = "5", mods = "CMD", action = act.ActivateTab(4) },
+	{ key = "6", mods = "CMD", action = act.ActivateTab(5) },
+	{ key = "7", mods = "CMD", action = act.ActivateTab(6) },
+	{ key = "8", mods = "CMD", action = act.ActivateTab(7) },
+	{ key = "9", mods = "CMD", action = act.ActivateTab(-1) },
+	{ key = "t", mods = "CMD", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "o", mods = "CMD", action = act.ActivateLastTab },
+	{ key = "w", mods = "CMD", action = act.CloseCurrentTab({ confirm = true }) },
+	{ key = "Z", mods = "SHIFT|CMD", action = act.TogglePaneZoomState },
+
+	{ key = "c", mods = "CMD", action = act.CopyTo("Clipboard") },
+	{ key = "v", mods = "CMD", action = act.PasteFrom("Clipboard") },
+	{ key = "x", mods = "CMD", action = act.ActivateCopyMode },
+
+	{ key = "_", mods = "SHIFT|CMD", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "|", mods = "SHIFT|CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+	{ key = "f", mods = "CMD", action = act.Search("CurrentSelectionOrEmptyString") },
+	{ key = "k", mods = "CMD", action = act.ClearScrollback("ScrollbackOnly") },
+
+	{ key = "p", mods = "CMD", action = act.PaneSelect({ alphabet = "", mode = "Activate" }) },
+	{ key = "e", mods = "CMD", action = act.CloseCurrentPane({ confirm = true }) },
+	-- { key = 'r',          mods = 'CMD',       action = act.RotatePanes("Clockwise") },
+
+	{ key = "q", mods = "CMD", action = act.QuitApplication },
+	{ key = "R", mods = "SHIFT|CMD", action = act.ReloadConfiguration },
+	{ key = "P", mods = "SHIFT|CMD", action = act.ActivateCommandPalette },
+	{ key = "L", mods = "SHIFT|CMD", action = act.ShowDebugOverlay },
+	{ key = "phys:Space", mods = "SHIFT|CMD", action = act.QuickSelect },
+	{
+		key = "U",
+		mods = "SHIFT|CMD",
+		action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }),
+	},
+
+	-- Launcher
+	{
+		mods = "SHIFT|CMD",
+		key = "Backspace",
+		action = act.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES|TABS|LAUNCH_MENU_ITEMS",
+		}),
+	},
+
+	-- resize window
+	{
+		key = "m",
+		mods = "CMD",
+		action = wezterm.action_callback(function(window, _, _)
+			window:maximize()
+		end),
+	},
+
+	{
+		key = "M",
+		mods = "SHIFT|CMD",
+		action = wezterm.action_callback(function(window, _, _)
+			window:restore()
+		end),
+	},
+
+	-- Rename tab
+	{
+		key = "T",
+		mods = "SHIFT|CMD",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, _pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+
+	-- Rename workspace
+	{
+		key = "W",
+		mods = "SHIFT|CMD",
+		action = act.PromptInputLine({
+			description = "Enter new name for workspace",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+				end
+			end),
+		}),
+	},
+
+	-- CTRL+SHIFT+Space, followed by 'r' will put us in resize-pane
+	-- mode until we cancel that mode.
+	{
+		key = "r",
+		mods = "CMD",
+		action = act.ActivateKeyTable({
+			name = "resize_pane",
+			one_shot = false,
+		}),
+	},
+
+	-- CTRL+SHIFT+Space, followed by 'a' will put us in activate-pane
+	-- mode until we press some other key or until 1 second (1000ms)
+	-- of time elapses
+	{
+		key = "a",
+		mods = "CMD",
+		action = act.ActivateKeyTable({
+			name = "activate_pane",
+			timeout_milliseconds = 1000,
+		}),
+	},
+}
+
+config.keys = keymaps
+
+config.key_tables = {
+	-- Defines the keys that are active in our resize-pane mode.
+	-- Since we're likely to want to make multiple adjustments,
+	-- we made the activation one_shot=false. We therefore need
+	-- to define a key assignment for getting out of this mode.
+	-- 'resize_pane' here corresponds to the name="resize_pane" in
+	-- the key assignments above.
+	resize_pane = {
+		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+
+		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+
+		-- Cancel the mode by pressing escape
+		{ key = "Escape", action = "PopKeyTable" },
+	},
+
+	-- Defines the keys that are active in our activate-pane mode.
+	-- 'activate_pane' here corresponds to the name="activate_pane" in
+	-- the key assignments above.
+	activate_pane = {
+		{ key = "LeftArrow", action = act.ActivatePaneDirection("Left") },
+		{ key = "h", action = act.ActivatePaneDirection("Left") },
+
+		{ key = "RightArrow", action = act.ActivatePaneDirection("Right") },
+		{ key = "l", action = act.ActivatePaneDirection("Right") },
+
+		{ key = "UpArrow", action = act.ActivatePaneDirection("Up") },
+		{ key = "k", action = act.ActivatePaneDirection("Up") },
+
+		{ key = "DownArrow", action = act.ActivatePaneDirection("Down") },
+		{ key = "j", action = act.ActivatePaneDirection("Down") },
+	},
+}
+
+config.launch_menu = {
+	{
+		label = "DevServer",
+		args = { "ssh", "root@192.168.0.243" },
+	},
+}
+
+config.hyperlink_rules = {
+	-- Linkify things that look like URLs and the host has a TLD name.
+	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+	{
+		regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
+		format = "$0",
+	},
+
+	-- linkify email addresses
+	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+	{
+		regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+		format = "mailto:$0",
+	},
+
+	-- file:// URI
+	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+	{
+		regex = [[\bfile://\S*\b]],
+		format = "$0",
+	},
+
+	-- Linkify things that look like URLs with numeric addresses as hosts.
+	-- E.g. http://127.0.0.1:8000 for a local development server,
+	-- or http://192.168.1.1 for the web interface of many routers.
+	{
+		regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+		format = "$0",
+	},
+
+	-- Make username/project paths clickable. This implies paths like the following are for GitHub.
+	-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+	-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+	{
+		regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+		format = "https://www.github.com/$1/$3",
+	},
+}
+
+-- Events
+
+wezterm.on("window-config-reloaded", function(window, pane)
+	window:toast_notification("wezterm", "configuration reloaded!", nil, 4000)
+end)
+
+-- Neovim Zen Mode Integration
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
+
+wezterm.on("new-tab-button-click", function(window, pane, button, default_action)
+	if default_action and button == "Left" then
+		window:perform_action(default_action, pane)
+	end
+
+	if default_action and button == "Right" then
+		window:perform_action(
+			wezterm.action.ShowLauncherArgs({
+				title = wezterm.nerdfonts.fa_rocket .. "  Select/Search:",
+				flags = "FUZZY|LAUNCH_MENU_ITEMS|DOMAINS",
+			}),
+			pane
+		)
+	end
+	return false
+end)
+
+wezterm.on("update-status", function(window, pane)
+	local cells = {}
+
+	if window:leader_is_active() then
+		local leader = "LEADER"
+		table.insert(cells, leader)
+	end
+
+	local active_key_table = window:active_key_table()
+	local mode = wezterm.format({
+		{ Text = active_key_table or "" },
+	})
+	if active_key_table then
+		table.insert(cells, mode)
+	end
+
+	local date = wezterm.strftime("%-d-%b-%Y %a")
+	table.insert(cells, date)
+
+	local time = wezterm.strftime("%r")
+	table.insert(cells, time)
+
+	local active_workspace = window:active_workspace()
+	local workspace = wezterm.format({
+		{ Text = wezterm.nerdfonts.md_dog_side .. " " .. active_workspace },
+	})
+	table.insert(cells, workspace)
+
+	local SOLID_LEFT_ARROW = ""
+	local FIRST_LEFT_ARROW = ""
+	-- Color palette for the backgrounds of each cell
+	local colors = {
+		"#3c1361",
+		"#52307c",
+		"#663a82",
+		"#7c5295",
+		"#b491c8",
+	}
+	-- Foreground color for the text across the fade
+	local text_fg = "#c0c0c0"
+
+	-- The elements to be formatted
+	local elements = {}
+	-- How many cells have been formatted
+	local num_cells = 0
+
+	-- Translate a cell into elements
+	local function push(text, is_last)
+		local cell_no = num_cells + 1
+		if cell_no == 1 then
+			table.insert(elements, { Foreground = { Color = colors[cell_no] } })
+			table.insert(elements, { Text = FIRST_LEFT_ARROW })
+		end
+		table.insert(elements, { Foreground = { Color = text_fg } })
+		table.insert(elements, { Background = { Color = colors[cell_no] } })
+		table.insert(elements, { Text = " " .. text .. " " })
+		if not is_last then
+			table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+			table.insert(elements, { Text = SOLID_LEFT_ARROW })
+		end
+		num_cells = num_cells + 1
+	end
+
+	while #cells > 0 do
+		local cell = table.remove(cells, 1)
+		push(cell, #cells == 0)
+	end
+
+	window:set_right_status(wezterm.format(elements))
+end)
+
+-- and finally, return the configuration to wezterm
+return config
